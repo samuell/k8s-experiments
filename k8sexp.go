481814ea12c2f2479e773aae90e5ef0b07fec359
Samuel Lampa <samuel.lampa@gmail.com>
2017-02-13 10:30:23 +0100

    Add some comments

--- k8sexp.go
+++ k8sexp.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/tools/clientcmd"
@@ -18,8 +19,13 @@ func main() {
 	check(err)
 	clientset, err := kubernetes.NewForConfig(config)
 	check(err)
+
+	// Access jobs. We can't do it all in one line, since we need to receive the
+	// errors and manage thgem appropriately
 	jobsList, err := clientset.BatchV1Client.Jobs("default").List(v1.ListOptions{})
 	check(err)
+
+	// Loop over all jobs and print their name
 	for i, job := range jobsList.Items {
 		fmt.Printf("Job %d: %s\n", i, job.Name)
 	}
